9824e05e149b90a61157fd3368198013b9f6533f
Arthur Andersen <leoc.git@gmail.com>
2012-12-19 12:03:42 +0100

    Remove debug output from dimmer

--- lib/rwave/devices/dimmer.rb
+++ lib/rwave/devices/dimmer.rb
@@ -16,12 +16,9 @@ module RWave
       end
 
       def on_request sent, received
-        puts
-        puts "Node-Handler received #{received}"
         if received.length > 4
           case received.bytes[3]
           when 0x13
-            puts "Currently running callback_ids: "+ @callback_ids.inspect
             callback_id = received.bytes[4]
             if @callback_ids.include?(callback_id)
               messenging_complete!(callback_id)
@@ -32,8 +29,6 @@ module RWave
 
     private
       def set_value val
-        puts
-        puts "Setting value for node 0x#{@node_id.to_s(16)}"
         bytes = [0x01, 0x0A, 0x00, 0x13, @node_id, 0x03, 0x20, 0x01, val, 0x05, 0x00, 0x00]
         send_message bytes, callback_id: get_callback_id
       end
