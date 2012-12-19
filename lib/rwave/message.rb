9f6908f4da0789b107d3470735423741263228ed
Arthur Andersen <leoc.git@gmail.com>
2012-12-19 12:04:25 +0100

    Refactor checksum method and add correctness check

--- lib/rwave/message.rb
+++ lib/rwave/message.rb
@@ -3,7 +3,10 @@ module RWave
     extend Forwardable
     def_delegator :@bytes, :length
 
-    ACK = [ 0x06 ]
+    SOF = 0x01
+    ACK = 0x06
+    NAK = 0x15
+    CAN = 0x18
 
     attr_accessor :callback_id, :bytes
 
@@ -23,7 +26,7 @@ module RWave
     def buf
       unless ack?
         bytes[bytes.length-2] = @callback_id if @callback_id
-        bytes[-1] = generate_checksum(bytes)
+        bytes[-1] = checksum
       end
       bytes.pack('C*')
     end
@@ -32,23 +35,39 @@ module RWave
     #
     # Returns boolean.
     def ack?
-      bytes == Message::ACK
+      bytes.first == 0x06
     end
 
     def to_s
-      "<Message: #{bytes.inspect}>"
+      "<Message: #{unsigned_bytes.map { |b| b.to_s(16).rjust(2, '0').upcase }.join(' ')}>"
+    end
+
+    def unsigned_bytes
+      bytes.map do |byte|
+        byte < 0 ? 256+byte : byte
+      end
+    end
+
+    def correct?
+      if bytes[0] == SOF
+        bytes[-1] == checksum
+      else
+        true
+      end
     end
 
     private
-    def generate_checksum data
-      bytes = data.clone
-      bytes = bytes[1...-1] # remove initial byte and checksum dummy
-      # fill ret with the first element to have something we can XOR with
-      ret = bytes.shift
-      bytes.each do |byte|
-        ret ^= byte
-      end
-      ~ret
+    # The checksum is calculated for the message as follows:
+    #  - remove first (SOF) and last (dummy) bytes
+    #  - execute XOR each byte of the message
+    #  - execute NOT on the result
+    #
+    # Because there is not byte type in ruby we have to make sure the
+    # result is an unsigned integer.
+    def checksum
+      data = bytes.clone[1...-1]
+      ret = ~data.inject(data.shift) { |acc, byte| acc ^ byte }
+      ret < 0 ? 256+ret : ret
     end
   end
 end
